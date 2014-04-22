3c60d6a02212e9963b2c6f1c0b6a274a6d7f4803
Nagy Atef <nagy.wesley@camelcasetech.com>
2014-04-22 12:40:01 +0200

    user module and listing

--- module/User/Module.php
+++ module/User/Module.php
@@ -1,6 +1,9 @@
 <?php
 namespace User;
-
+ use User\Model\User;
+ use User\Model\UserTable;
+ use Zend\Db\ResultSet\ResultSet;
+ use Zend\Db\TableGateway\TableGateway;
 class Module
 {
     public function getConfig()
@@ -18,4 +21,22 @@ class Module
             ),
         );
     }
+     public function getServiceConfig()
+     {
+         return array(
+             'factories' => array(
+                 'User\Model\UserTable' =>  function($sm) {
+                     $tableGateway = $sm->get('UserTableGateway');
+                     $table = new  UserTable($tableGateway);
+                     return $table;
+                 },
+                 'UserTableGateway' => function ($sm) {
+                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                     $resultSetPrototype = new ResultSet();
+                     $resultSetPrototype->setArrayObjectPrototype(new User());
+                     return new TableGateway('user', $dbAdapter, null, $resultSetPrototype);
+                 },
+             ),
+         );
+     }
 }
--- module/User/src/User/Controller/UserController.php
+++ module/User/src/User/Controller/UserController.php
@@ -4,14 +4,41 @@ namespace User\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use User\Model\UserTable;
+use User\Model\User;
 
 class UserController extends AbstractActionController
 {
+    protected $userTable;
 
     public function indexAction()
     {
+        $users =$this->getUserTable()->fetchAll();
+         return new ViewModel(array(
+             'users' => $this->getUserTable()->fetchAll(),
+         ));
+    }
+
+    public function addAction()
+    {
         return new ViewModel();
     }
+
+    public function editAction()
+    {
+        return new ViewModel();
+    }
+      public function getUserTable()
+     {
+         
+         if (!$this->userTable) {
+             $sm = $this->getServiceLocator();
+             $this->userTable = $sm->get('User\Model\UserTable');
+             
+         }
+         
+         return $this->userTable;
+     }
 
 
 }
